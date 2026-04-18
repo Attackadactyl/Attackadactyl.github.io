f37cb1abd77b2547ca1af3364a1ec3d036667bea
alexrwalker <alex@epochgeo.com>
2026-04-17 23:12:44 -0400

    edits to try and get mobile working alright

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const buildUrl = "Build";
-const config = {
-  dataUrl:            `${buildUrl}/Web.data`,
-  frameworkUrl:       `${buildUrl}/Web.framework.js`,
-  codeUrl:            `${buildUrl}/Web.wasm`,
-  streamingAssetsUrl: "StreamingAssets",
-  companyName:        "YourCompany",
-  productName:        "Circulorb",
-  productVersion:     "1.0",
-};
-
-createUnityInstance(document.querySelector("#unity-canvas"), config);
+// Unity boot logic is in index.html
